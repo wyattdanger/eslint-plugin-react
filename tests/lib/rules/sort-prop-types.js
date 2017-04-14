84f82f532300a2397fe7c6256cdbd7e641337027
Stephen Wyatt Bush <stephen.bush@airbnb.com>
2017-04-14 13:33:31 -0700

    Revert "Add support for variable reference to sort-prop-types (fixes #622)"
    This reverts commit 13806f1bc99817c06223a2b799bdc1125d208290.

--- tests/lib/rules/sort-prop-types.js
+++ tests/lib/rules/sort-prop-types.js
@@ -323,15 +323,6 @@ ruleTester.run('sort-prop-types', rule, {
       '}'
     ].join('\n'),
     parser: 'babel-eslint'
-  }, {
-    code: [
-      'const propTypes = require(\'./externalPropTypes\')',
-      'const TextFieldLabel = (props) => {',
-      '  return <div />;',
-      '};',
-      'TextFieldLabel.propTypes = propTypes;'
-    ].join('\n'),
-    parserOptions: parserOptions
   }],
 
   invalid: [{
@@ -631,24 +622,6 @@ ruleTester.run('sort-prop-types', rule, {
       message: 'Prop types declarations should be sorted alphabetically',
       line: 6,
       column: 5,
-      type: 'Property'
-    }]
-  }, {
-    code: [
-      'const propTypes = {',
-      '  b: PropTypes.string,',
-      '  a: PropTypes.string,',
-      '};',
-      'const TextFieldLabel = (props) => {',
-      '  return <div />;',
-      '};',
-      'TextFieldLabel.propTypes = propTypes;'
-    ].join('\n'),
-    parserOptions: parserOptions,
-    errors: [{
-      message: ERROR_MESSAGE,
-      line: 3,
-      column: 3,
       type: 'Property'
     }]
   }]
